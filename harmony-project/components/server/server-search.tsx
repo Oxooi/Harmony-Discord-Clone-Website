ba4e8ba29b90a9e3cb4cb415565db9d79d27a955
Oxooi <86341288+Oxooi@users.noreply.github.com>
2023-12-19 02:44:03 +0100

    Change the display of : CTRL K

--- harmony-project/components/server/server-search.tsx
+++ harmony-project/components/server/server-search.tsx
@@ -49,7 +49,12 @@ export const ServerSearch = ({
                 <kbd
                     className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto"
                 >
-                    <span className="text-xs">CTRL</span>K
+                    <span className="text-xs">CTRL</span>
+                </kbd>
+                <kbd
+                    className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground"
+                >
+                    <span className="text-xs">K</span>
                 </kbd>
             </button>
             <CommandDialog open={open} onOpenChange={setOpen}>
