73cca57cc4672231910ac3e5237e8050a20b5879
Oxooi <86341288+Oxooi@users.noreply.github.com>
2023-12-24 11:55:01 +0100

    add text files in upload files

--- harmony-project/components/file-upload.tsx
+++ harmony-project/components/file-upload.tsx
@@ -62,6 +62,20 @@ export const FileUpload = ({
             </div>
         )
     }
+    if (value && fileType === "txt") {
+        return (
+            <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+                <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+                <button
+                    onClick={() => onChange("")}
+                    className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+                    type="button"
+                >
+                    <X className="h-4 w-4" />
+                </button>
+            </div>
+        )
+    }
 
     return (
         <UploadDropzone
