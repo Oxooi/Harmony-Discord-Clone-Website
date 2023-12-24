d762abb9898bf4ae5e095a9794405ce221d2ba33
Oxooi <86341288+Oxooi@users.noreply.github.com>
2023-12-24 21:12:09 +0100

    Auto refresh the messages

--- harmony-project/components/chat/chat-messages.tsx
+++ harmony-project/components/chat/chat-messages.tsx
@@ -1,4 +1,4 @@
-'use client'
+ 'use client'
 
 import { Member, Message, Profile } from '@prisma/client'
 import { ChatWelcome } from './chat-welcome'
@@ -8,6 +8,7 @@ import { Skeleton } from '../ui/skeleton'
 import { Fragment } from 'react'
 import {format} from "date-fns"
 import { ChatItem } from './chat-item'
+import { useChatSocket } from '@/hooks/use-chat-socket'
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
@@ -41,6 +42,8 @@ export const ChatMessages = ({
   type,
 }: ChatMessagesProps) => {
   const queryKey = `chat:${chatId}`; 
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`;
 
   const {
     data,
@@ -54,6 +57,7 @@ export const ChatMessages = ({
     paramKey,
     paramValue,
   }); 
+  useChatSocket({ queryKey, addKey, updateKey});
 
   if (status === "pending") {
     return(
